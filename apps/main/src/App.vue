f88d6dad62f6a1147dfd4168a25636df3a236076
Shaowei SW5 Guo <guosw5@lenovo.com>
2025-04-18 20:28:58 +0800

    1

--- apps/main/src/App.vue
+++ apps/main/src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <h1>Hello, Vue 2 + Vite!</h1>
     <el-button>click</el-button>
+    <el-input placeholder="Please input..."></el-input>
   </div>
 </template>
 
@@ -10,14 +11,3 @@ export default {
   name: 'App',
 };
 </script>
-
-<style scoped>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- apps/theme-chalk/gulpfile.js
+++ apps/theme-chalk/gulpfile.js
@@ -1,27 +1,25 @@
 'use strict';
 
 const { series, src, dest } = require('gulp');
-const dartSass = require('sass');
-const gulpSass = require('gulp-sass');
-const sass = gulpSass(dartSass);
+const sass = require('gulp-dart-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const cssmin = require('gulp-cssmin');
 
 function compile() {
   return src('./src/*.scss')
     .pipe(sass.sync().on('error', sass.logError))
-    .pipe(
-      autoprefixer({
-        overrideBrowserslist: ['ie > 9', 'last 2 versions'],
-        cascade: false,
-      })
-    )
+    .pipe(autoprefixer({
+      overrideBrowserslist: ['ie > 9', 'last 2 versions'],
+      cascade: false
+    }))
     .pipe(cssmin())
     .pipe(dest('./lib'));
 }
 
 function copyfont() {
-  return src('./src/fonts/**').pipe(cssmin()).pipe(dest('./lib/fonts'));
+  return src('./src/fonts/**')
+    .pipe(cssmin())
+    .pipe(dest('./lib/fonts'));
 }
 
 exports.build = series(compile, copyfont);

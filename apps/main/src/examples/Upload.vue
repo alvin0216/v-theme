1a4d43a8007ea8bea5028c9dc37c21ba90400066
Shaowei SW5 Guo <guosw5@lenovo.com>
2025-04-19 12:30:45 +0800

    111

---
+++ apps/main/src/examples/Upload.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <h2 id="Upload">Upload 上传</h2>
+
+    <div class="example">
+      <el-row>
+        <el-upload :action="action" :file-list="fileList">
+          <el-button type="primary">点击上传</el-button>
+        </el-upload>
+      </el-row>
+
+      <el-row>
+        <el-upload :action="action" :file-list="fileList" :on-preview="onPreview" list-type="picture-card">
+          <i class="el-icon-plus" />
+        </el-upload>
+      </el-row>
+
+      <el-row>
+        <el-upload :action="action" :file-list="fileList" list-type="picture">
+          <el-button size="small" type="primary">点击上传</el-button>
+          <p slot="tip">只能上传jpg/png文件，且不超过500kb</p>
+        </el-upload>
+      </el-row>
+
+      <el-row>
+        <el-upload :action="action" :file-list="fileList" drag>
+          <i class="el-icon-upload" />
+          <div class="el-upload__text">
+            将文件拖到此处，或
+            <em>点击上传</em>
+          </div>
+          <p slot="tip">只能上传jpg/png文件，且不超过500kb</p>
+        </el-upload>
+      </el-row>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Upload',
+  data() {
+    return {
+      action: 'https://jsonplaceholder.typicode.com/posts/',
+      fileList: [
+        {
+          name: 'food.jpeg',
+          url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100',
+        },
+      ],
+    }
+  },
+  methods: {
+    onPreview() {},
+  },
+}
+</script>
---
+++ apps/main/src/examples/index.vue
@@ -0,0 +1,178 @@
+<template>
+  <div class="content">
+    <alert-example />
+    <autocomplete-example />
+    <avatar-example />
+    <backtop-example />
+    <badge-example />
+    <breadcrumb-example />
+    <button-example />
+    <calendar-example />
+    <card-example />
+    <carousel-example />
+    <cascader-example />
+    <checkbox-example />
+    <collapse-example />
+    <color-picker-example />
+    <date-picker-example />
+    <descriptions-example />
+    <dialog-example />
+    <divider-example />
+    <drawer-example />
+    <dropdown-example />
+    <empty-example />
+    <form-example />
+    <image-example />
+    <input-number-example />
+    <input-example />
+    <link-example />
+    <loading-example />
+    <menu-example />
+    <message-box-example />
+    <message-example />
+    <notification-example />
+    <pageHeader-example />
+    <pagination-example />
+    <popconfirm-example />
+    <popover-example />
+    <progress-example />
+    <radio-example />
+    <rate-example />
+    <result-example />
+    <scrollbar-example />
+    <select-example />
+    <skeleton-example />
+    <slider-example />
+    <statistic-example />
+    <steps-example />
+    <switch-example />
+    <table-example />
+    <tabs-example />
+    <tag-example />
+    <time-picker-example />
+    <timeline-example />
+    <tooltip-example />
+    <transfer-example />
+    <tree-example />
+    <upload-example />
+  </div>
+</template>
+
+<script>
+import AlertExample from './Alert.vue';
+import AutocompleteExample from './Autocomplete.vue';
+import AvatarExample from './Avatar.vue';
+import BacktopExample from './Backtop.vue';
+import BadgeExample from './Badge.vue';
+import BreadcrumbExample from './Breadcrumb.vue';
+import ButtonExample from './Button.vue';
+import CalendarExample from './Calendar.vue';
+import CardExample from './Card.vue';
+import CarouselExample from './Carousel.vue';
+import CascaderExample from './Cascader.vue';
+import CheckboxExample from './Checkbox.vue';
+import CollapseExample from './Collapse.vue';
+import ColorPickerExample from './ColorPicker.vue';
+import DatePickerExample from './DatePicker.vue';
+import DescriptionsExample from './Descriptions.vue';
+import DialogExample from './Dialog.vue';
+import DividerExample from './Divider.vue';
+import DrawerExample from './Drawer.vue';
+import DropdownExample from './Dropdown.vue';
+import EmptyExample from './Empty.vue';
+import FormExample from './Form.vue';
+import ImageExample from './Image.vue';
+import InputNumberExample from './InputNumber.vue';
+import InputExample from './Input.vue';
+import LinkExample from './Link.vue';
+import LoadingExample from './Loading.vue';
+import MenuExample from './Menu.vue';
+import MessageBoxExample from './MessageBox.vue';
+import MessageExample from './Message.vue';
+import NotificationExample from './Notification.vue';
+import PageHeaderExample from './PageHeader.vue';
+import PaginationExample from './Pagination.vue';
+import PopconfirmExample from './Popconfirm.vue';
+import PopoverExample from './Popover.vue';
+import ProgressExample from './Progress.vue';
+import RadioExample from './Radio.vue';
+import RateExample from './Rate.vue';
+import ResultExample from './Result.vue';
+import ScrollbarExample from './Scrollbar.vue';
+import SelectExample from './Select.vue';
+import SkeletonExample from './Skeleton.vue';
+import SliderExample from './Slider.vue';
+import StatisticExample from './Statistic.vue';
+import StepsExample from './Steps.vue';
+import SwitchExample from './Switch.vue';
+import TableExample from './Table.vue';
+import TabsExample from './Tabs.vue';
+import TagExample from './Tag.vue';
+import TimePickerExample from './TimePicker.vue';
+import TimelineExample from './Timeline.vue';
+import TooltipExample from './Tooltip.vue';
+import TransferExample from './Transfer.vue';
+import TreeExample from './Tree.vue';
+import UploadExample from './Upload.vue';
+
+export default {
+  name: 'Examples',
+  components: {
+    AlertExample,
+    AutocompleteExample,
+    AvatarExample,
+    BacktopExample,
+    BadgeExample,
+    BreadcrumbExample,
+    ButtonExample,
+    CalendarExample,
+    CardExample,
+    CarouselExample,
+    CascaderExample,
+    CheckboxExample,
+    CollapseExample,
+    ColorPickerExample,
+    DatePickerExample,
+    DescriptionsExample,
+    DialogExample,
+    DividerExample,
+    DrawerExample,
+    DropdownExample,
+    EmptyExample,
+    FormExample,
+    ImageExample,
+    InputNumberExample,
+    InputExample,
+    LinkExample,
+    LoadingExample,
+    MenuExample,
+    MessageBoxExample,
+    MessageExample,
+    NotificationExample,
+    PageHeaderExample,
+    PaginationExample,
+    PopconfirmExample,
+    PopoverExample,
+    ProgressExample,
+    RadioExample,
+    RateExample,
+    ResultExample,
+    ScrollbarExample,
+    SelectExample,
+    SkeletonExample,
+    SliderExample,
+    StatisticExample,
+    StepsExample,
+    SwitchExample,
+    TableExample,
+    TabsExample,
+    TagExample,
+    TimePickerExample,
+    TimelineExample,
+    TooltipExample,
+    TransferExample,
+    TreeExample,
+    UploadExample,
+  },
+};
+</script>

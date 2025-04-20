ea2d03ee74ca5853b88ba8009982dea367b7f3f7
Shaowei SW5 Guo <guosw5@lenovo.com>
2025-04-20 17:00:51 +0800

    32

--- apps/main/src/App.vue
+++ apps/main/src/App.vue
@@ -13,7 +13,7 @@
 export default {
   name: 'App',
   components: {
-    Example: () => import('./examples/Dropdown.vue'),
+    Example: () => import('./examples/Dialog.vue'),
   },
   methods: {
     toggle() {

d732af7b00ba47fd337982c0d03de312070e2085
Shaowei SW5 Guo <guosw5@lenovo.com>
2025-04-21 21:47:21 +0800

    111

--- apps/theme-dark/.prettierrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  semi: false,
-  printWidth: 110,
-  arrowParens: 'avoid',
-  singleQuote: true,
-  endOfLine: 'crlf',
-}
--- apps/theme-dark/scripts/utils/index.js
+++
@@ -1,8 +0,0 @@
-export function insertStyleElement(content) {
-  if (typeof window === 'undefined') return
-
-  const style = document.createElement('style')
-
-  style.appendChild(document.createTextNode(content))
-  document.head.appendChild(style)
-}

eaac37e33bb3ff4651f34dae2aa0a2a38222664e
Shaowei SW5 Guo <guosw5@lenovo.com>
2025-04-25 00:16:37 +0800

    323

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@
 export default {
   name: 'App',
   components: {
-    Example: () => import('./examples/Link.vue'),
+    Example: () => import('./examples/Dropdown.vue'),
   },
   methods: {
     toggle() {
